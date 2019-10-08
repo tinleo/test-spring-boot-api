6688795f2231e9f5532804ff896ea86c8b693b1d
tinledotrung <tin.le@netcompany.com>
2019-10-08 08:27:59 +0100

    test api

---
+++ src/main/java/com/example/testspringbootapi/TestspringbootapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.testspringbootapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestspringbootapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestspringbootapiApplication.class, args);
+	}
+
+}
